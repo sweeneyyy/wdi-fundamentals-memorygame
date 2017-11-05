d039a0c8b77fd96f6522876bf4c9e2e0bfedb671
Sweeney Arnett <sweeneyarnett@gmail.com>
2017-11-05 17:20:51 -0500

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 //check for match
 var checkForMatch = function (){
@@ -10,8 +32,10 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 //when user flips, add that card to array of cards in play. if flipped two cards, check for match.
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId]);  
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);  
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	//find number of cards flipped
 	if (cardsInPlay.length === 2) {
 	checkForMatch();	
