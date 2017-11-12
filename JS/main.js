996dc27cf4192be612e1dd3caaa386470ebb70a7
Sweeney Arnett <sweeneyarnett@gmail.com>
2017-11-12 13:44:05 -0500

    final with reset button

--- JS/main.js
+++ JS/main.js
@@ -1,49 +1,66 @@
 var cards = [
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-    rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
-}
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+	    rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
 ];
 
 var cardsInPlay = [];
-//check for match
-var checkForMatch = function (){
-if (cardsInPlay[0] === cardsInPlay[1]) {
+// check for match
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
-} else {
+  } else {
 	alert("Sorry, try again.");	
   }
 }
-//when user flips, add that card to array of cards in play. if flipped two cards, check for match.
-var flipCard = function (cardId) {
+// when user flips, add that card to array of cards in play
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);  
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	//find number of cards flipped
-	if (cardsInPlay.length === 2) {
-	checkForMatch();	
-  }
-}
-flipCard(0);
-flipCard(2);
+	this.setAttribute('src', cards[cardId].cardImage)
 
+	// find number of cards flipped, if two check for match
+	if (cardsInPlay.length === 2) {
+	  checkForMatch();	
+    }
+}
+// add each card to the board
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+var resetBoard = function () {
+    location.reload();
+}
+document.getElementById("reset").addEventListener('click', resetBoard);
+
+createBoard();
 
 
 
